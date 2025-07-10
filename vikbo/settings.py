293e3163327a5e689bc27bfda112967e59589c75
Alfred <alfred@stensatter.se>
2025-07-10 16:41:15 +0200

    works with bug (sends confirmation to both)

--- vikbo/settings.py
+++ vikbo/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "website",
     "events",
+    "mail",
 
 ]
 
@@ -133,3 +134,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+INBOX_EMAIL = os.environ['INBOX_EMAIL']
